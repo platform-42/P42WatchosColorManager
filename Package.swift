f0b50f8cb4cfc9ac6010960c7eed9e7c652e18c7
platform-42 <142492114+platform-42@users.noreply.github.com>
2026-03-13 16:55:17 +0100

    dddd

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "P42WatchosColormManager",
+    name: "P42WatchosColorManager",
     platforms: [
         .watchOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "P42WatchosColormManager",
-            targets: ["P42WatchosColormManager"]
+            name: "P42WatchosColorManager",
+            targets: ["P42WatchosColorManager"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "P42WatchosColormManager"
+            name: "P42WatchosColorManager"
         ),
 
     ]
